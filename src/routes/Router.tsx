f11e4f833539240fa966f99bd6e396f8e224cf2c
0xATHERIS <ath3ris@proton.me>
2023-03-28 16:44:10 +0100

    Router

---
+++ src/routes/Router.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from '../Home';
+import Layout from '../layout/Layout';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
