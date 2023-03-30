6fab4e19c8fddc1b5eb54a2eb8659b3b94690473
0xATHERIS <ath3ris@proton.me>
2023-03-30 01:25:45 +0100

    feat: Started Homepage -

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import reactLogo from './assets/react.svg'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='max-w-[1170px] mx-auto flex flex-col items-center gap-[100px]'>
+      <h4 className='text-black font-sans text-2xl tracking-wider w-[600px] text-center mt-[90px]'>
+        Hey, I'm <span className='font-cals'>Andrew</span>. A Frontend developer from a town in the middle of Liverpool and Manchester called Warrington.
+      </h4>
+      <h4 className='text-black text-2xl tracking-wider text-center'>
+        My Skills, Passion and Experience
+      </h4>
+      <img src={reactLogo} className="logo react" alt="React logo" />
+    </div>
   )
 }
 
