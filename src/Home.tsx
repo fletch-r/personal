26360ff1a970b99f4963d61ed8dd1d331704852a
0xATHERIS <ath3ris@proton.me>
2023-04-12 23:58:11 +0100

    feat: :tada:  - Started main page of website

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,15 +1,40 @@
-import reactLogo from './assets/react.svg'
+import kaiba from './assets/kaiba.jpeg';
 
 function Home() {
   return (
-    <div className='max-w-[1170px] mx-auto flex flex-col items-center gap-[100px]'>
-      <h4 className='text-black font-sans text-2xl tracking-wider w-[600px] text-center mt-[90px]'>
-        Hey, I'm <span className='font-cals'>Andrew</span>. A Frontend developer from a town in the middle of Liverpool and Manchester called Warrington.
-      </h4>
-      <h4 className='text-black text-2xl tracking-wider text-center'>
-        My Skills, Passion and Experience
-      </h4>
-      <img src={reactLogo} className="logo react" alt="React logo" />
+    <div className='max-w-[945px] mx-auto flex flex-col items-center gap-[100px]'>
+      <div className="w-full flex justify-between items-center">
+        <div className="flex flex-col gap-6 w-[545px]">
+          <h1 className="text-4xl text-white">Hey, I'm <span>Andrew Fletcher</span></h1>
+          <div className="flex flex-col gap-3">
+            <p className="text-lg text-white">I'm a Frontend developer from a town in the middle of Liverpool and Manchester called Warrington.</p>
+            <p className="text-lg text-white">My passions are everything frontend, UI and UX. I love to code and help out on open source UI projects.</p>
+          </div>
+        </div>
+        <div>
+          <div className="w-[200px] h-[200px] bg-neutral-50 rounded-full flex items-center justify-center overflow-hidden border border-denim-500 shadow-md shadow-denim-400">
+            <img src={kaiba} alt="kaiba" />
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full">
+        <h4 className="text-white text-2xl font-cals mb-5">Projects</h4>
+        <div className="w-full flex gap-6">
+          <a href="/simple-commit" className="border border-2 border-oxford-main hover:border-denim-main hover:bg-denim-main bg-oxford-main bg-opacity-10 hover:bg-opacity-10 p-4 rounded-lg w-1/3">
+            <h4 className="text-xl text-white mb-3 font-bold">Simple Commit</h4>
+            <p className="text-white">This is a description about Simple Commit. I can just take it from the README as I can't remember what I wrote.</p>
+          </a>
+          <a href="/content-editor" className="border border-2 border-oxford-main hover:border-denim-main hover:bg-denim-main bg-oxford-main bg-opacity-10 hover:bg-opacity-10 p-4 rounded-lg w-1/3">
+            <h4 className="text-xl text-white mb-3 font-bold">Content Editor</h4>
+            <p className="text-white">This is a description about the Content Editor and how it is built in Vuejs and manages JD’s content.</p>
+          </a>
+          <a href="/prism" className="border border-2 border-oxford-main hover:border-denim-main hover:bg-denim-main bg-oxford-main bg-opacity-10 hover:bg-opacity-10 p-4 rounded-lg w-1/3">
+            <h4 className="text-xl text-white mb-3 font-bold">PRISM</h4>
+            <p className="text-white">This is a description about PRISM and how it helps JD manage its millions of products.</p>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,48 +1,35 @@
-import { ReactNode, useState } from 'react'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { Outlet } from 'react-router-dom';
 
 function Layout() {
-    const [projectsHovered, setProjectsHovered] = useState(false);
+    // const [projectsHovered, setProjectsHovered] = useState(false);
   return (
     <>
-      <nav className='bg-black h-[100px] flex items-stretch z-50 relative'>
-        <div className='max-w-[1170px] mx-auto flex justify-between items-end w-full'>
-            <div className='flex justify-center items-center h-full'>
-                <h4 className='text-white text-xl'>Andrew Fletcher</h4>
-            </div>
-            <ul className='flex justify-around items-stretch gap-8 h-2/3 pt-1'>
-                <a
-                    href='/projects'
-                    className='hover:bg-[#FFDD00] text-white hover:text-black px-4 w-[140px] flex justify-center'
-                    onMouseEnter={() => setProjectsHovered(true)}
-                    onMouseLeave={() => setProjectsHovered(false)}
-                >
+      <nav className='bg-steal-950 h-[50px] flex items-stretch z-50 relative border-b border-oxford-main mb-20'>
+        <div className="max-w-[1100px] mx-auto flex justify-between items-center w-full">
+            <h4 className='text-white text-xl font-cals'>Andrew</h4>
+            <ol className='flex gap-20 h-2/3 pt-1'>
+                <a href='/about'>
                     <li>
-                        <h5>Projects</h5>
+                        <h5 className='text-white font-inter'>About</h5>
                     </li>
                 </a>
-                <a href='/cv' className='hover:bg-[#FFDD00] text-white hover:text-black px-4 w-[140px] flex justify-center'>
+                <a href='/projects'>
                     <li>
-                        <h5>CV</h5>
+                        <h5 className='text-white font-inter'>Projects</h5>
                     </li>
                 </a>
-                <a href='/contact' className='hover:bg-[#FFDD00] text-white hover:text-black px-4 w-[140px] flex justify-center'>
-                    <li>
-                        <h5>Contact Me</h5>
-                    </li>
-                </a>
-                <a href='https://github.com/0xATHERIS' target='_blank' className='text-white px-4 w-[80px] flex justify-center'>
+                <a href='https://github.com/0xATHERIS' target='_blank' className='text-white'>
                     <li>
                         <GitHubIcon />
                     </li>
                 </a>
-            </ul>
+            </ol>
         </div>
       </nav>
-      <div className={`z-20 h-[200px] bg-black shadow absolute transition-all hover:top-[100px] left-0 w-full ${projectsHovered ? 'top-[100px]' : '-top-[100%]'}`}>
+      {/* <div className={`z-20 h-[200px] bg-black shadow absolute transition-all hover:top-[100px] left-0 w-full ${projectsHovered ? 'top-[100px]' : '-top-[100%]'}`}>
         <p className='text-white'>This is where the projects are going to be</p>
-      </div>
+      </div> */}
       <main>
         <Outlet />
       </main>
