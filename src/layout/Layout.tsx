dccbd5f7774177a096733997afe030a9e8e9d2ab
0xATHERIS <ath3ris@proton.me>
2023-04-13 00:35:55 +0100

    feat: :sparkles:  - Added footer content

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,4 +1,6 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { Outlet } from 'react-router-dom';
 
 function Layout() {
@@ -40,7 +42,20 @@ function Layout() {
       <main>
         <Outlet />
       </main>
-      <footer></footer>
+      <footer className='max-w-[1100px] mx-auto border-t border-oxford-main p-4 mt-40 flex justify-between'>
+        <p className="text-steal-400">Andrew Fletcher © 2023</p>
+        <div className='flex gap-8'>
+            <a href="https://www.twitter.com/httpfleck">
+                <TwitterIcon />
+            </a>
+            <a href="https://uk.linkedin.com/in/andrew-fletcher-3009">
+                <LinkedInIcon />
+            </a>
+            <a href="https://github.com/0xATHERIS">
+                <GitHubIcon />
+            </a>
+        </div>
+      </footer>
     </>
   )
 }
