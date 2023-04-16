620b91d278ee38c721fe135320c8356a3284a834
0xATHERIS <ath3ris@proton.me>
2023-04-16 23:52:31 +0100

    style: :lipstick:  - page width styling

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -6,29 +6,29 @@ import { Outlet } from 'react-router-dom';
 function Layout() {
     // const [projectsHovered, setProjectsHovered] = useState(false);
   return (
-    <>
-      <nav className='bg-steal-950 h-[50px] flex items-stretch z-50 relative border-b border-oxford-main mb-20'>
+    <div>
+      <nav className='h-[50px] flex items-stretch z-50 relative border-b border-steal-100 mb-20 dark:border-oxford-main'>
         <div className="max-w-[1100px] mx-auto flex justify-between items-center w-full">
             <a href='/'>
-                <h4 className='text-white text-xl font-cals'>Andrew</h4>
+                <h4 className='text-xl font-cals'>Andrew</h4>
             </a>
             <ol className='flex gap-20 h-2/3 pt-1'>
                 <a href='/about'>
                     <li>
-                        <h5 className='text-white font-inter'>About</h5>
+                        <h5 className='font-inter'>About</h5>
                     </li>
                 </a>
                 <a href='/projects'>
                     <li>
-                        <h5 className='text-white font-inter'>Projects</h5>
+                        <h5 className='font-inter'>Projects</h5>
                     </li>
                 </a>
                 <a href='/cv'>
                     <li>
-                        <h5 className='text-white font-inter'>CV</h5>
+                        <h5 className='font-inter'>CV</h5>
                     </li>
                 </a>
-                <a href='https://github.com/0xATHERIS' target='_blank' className='text-white'>
+                <a href='https://github.com/0xATHERIS' target='_blank'>
                     <li>
                         <GitHubIcon />
                     </li>
@@ -36,10 +36,10 @@ function Layout() {
             </ol>
         </div>
       </nav>
-      <main>
+      <main className='h-full max-w-[945px] mx-auto'>
         <Outlet />
       </main>
-      <footer className='max-w-[1100px] mx-auto border-t border-oxford-main px-4 py-6 mt-20 flex justify-between items-center'>
+      <footer className='max-w-[1100px] mx-auto border-t border-steal-100 dark:border-oxford-main px-4 py-6 flex justify-between items-center'>
         <p className="text-neutral-500">Andrew Fletcher © 2023</p>
         <div className='flex gap-8'>
             <a href="https://www.twitter.com/httpfleck">
@@ -53,7 +53,7 @@ function Layout() {
             </a>
         </div>
       </footer>
-    </>
+    </div>
   )
 }
 
