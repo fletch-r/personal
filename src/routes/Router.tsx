cdab6384660bb3054daf6e6a6e4360c6bed80773
0xATHERIS <ath3ris@proton.me>
2023-04-20 01:33:32 +0100

    feat: :sparkles:  - Removed comments and added About page

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from '../Home';
 import Layout from '../layout/Layout';
+import About from '../pages/About';
 
 function Router() {
   return (
@@ -9,8 +9,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          {/* <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} /> */}
+          <Route path="about" element={<About />} />
           <Route path="*" element={<h1 className='ml-40'>Coming Soon</h1>} />
         </Route>
       </Routes>
