65b0e84e8b18c5814f465c7bba03ade3f0e358b9
0xATHERIS <ath3ris@proton.me>
2023-04-25 21:44:15 +0100

    feat: :sparkles:  - component containing default styling for code elements

---
+++ src/components/Code.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type CodeProps = {
+    children: React.ReactNode
+}
+
+function Code({ children }: CodeProps) {
+  return (
+    <code className="border rounded px-2 border-steal-500 py-1 text-sm">
+        {children}
+    </code>
+  )
+}
+
+export default Code
