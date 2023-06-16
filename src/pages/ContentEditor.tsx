78fb34409479dc4a970edb6614eb18fa6945d855
0xATHERIS <ath3ris@proton.me>
2023-06-16 23:54:19 +0100

    feat: :sparkles:  - started content editor page

---
+++ src/pages/ContentEditor.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ContentEditor() {
+    return (
+        <article className='prose prose-invert tracking-wider leading-none mx-auto'>
+            <h1 className="font-cals">Content Editor</h1>
+            <p className="lead">
+                A CMS where users can build and change the content of pages.
+            </p>
+            <p>
+                The Content Editor was the first project I was assigned when joining JDSports.
+
+            </p>
+        </article>
+    )
+}
+
+export default ContentEditor
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -6,6 +6,7 @@ import About from '../pages/About';
 import SimpleCommit from "../pages/SimpleCommit";
 import RadixTable from "../pages/RadixTable";
 import CRI from "../pages/CRI";
+import ContentEditor from "../pages/ContentEditor";
 
 function Router() {
   return (
@@ -17,6 +18,7 @@ function Router() {
           <Route path="simple-commit" element={<SimpleCommit />} />
           <Route path="radix-table" element={<RadixTable />} />
           <Route path="create-react-interface" element={<CRI />} />
+          <Route path="content-editor" element={<ContentEditor />} />
           <Route path="*" element={<h1 className='ml-40'>Coming Soon</h1>} />
         </Route>
       </Routes>
