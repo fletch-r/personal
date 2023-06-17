c4c03fae195d63436fa0e6340b1c6ebce4e084ef
0xATHERIS <ath3ris@proton.me>
2023-06-17 14:10:48 +0100

    feat: :sparkles:  - Card component which displays content editor component examples

---
+++ src/components/ComponentCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+type ComponentCardProps = {
+    label: string;
+    selected: boolean;
+    onClick: () => void;
+    children: React.ReactElement;
+}
+
+function ComponentCard({ onClick, selected, label, children }: ComponentCardProps) {
+    return (
+        <button
+            type="button"
+            onClick={() => onClick()}
+            className={`
+            w-1/4 h-[180px]
+            bg-neutral-700
+            border ${selected ? 'border-inherit' : 'border-transparent'} hover:border-inherit
+            rounded-lg
+            p-2
+            flex flex-col items-center justify-evenly
+            cursor-pointer
+        `}
+        >
+            <div className='w-full h-3/4 flex items-center justify-center'>
+                {children}
+            </div>
+            <div className="w-full h-1/4 flex items-center justify-center">
+                <p>{label}</p>
+            </div>
+        </button>
+    )
+}
+
+export default ComponentCard
